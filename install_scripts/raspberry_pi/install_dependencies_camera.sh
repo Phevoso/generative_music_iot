6bc5551585440214a96c14f5d672e0814211cb17
tr3m <tr3m@manos>
2022-12-01 23:57:13 +0200

    Added camera installer script

--- install_scripts/raspberry_pi/install_dependencies_camera.sh
+++ install_scripts/raspberry_pi/install_dependencies_camera.sh
@@ -23,7 +23,7 @@ readc()
 }
 
 sudo clear
-echo -e "\e[1;33mWARNING: This script installs all package and python library dependencies for this project.\nYou should be using this on the Raspberry Pi running the sensors.\n\nPackage and Library dependenices are listed in the install_scripts directory's README if you want to install them manually.\n\nPress any key to continue or ESC to exit...\033[0m"
+echo -e "\e[1;33mWARNING: This script installs all package and python library dependencies for this project.\nYou should be using this on the Raspberry Pi running the camera.\n\nAdditionally, the camera functionality of the Raspberry Pi must be enabled.\n(You can enable the camera functionality by using \"sudo raspi-config\")\n\nPackage and Library dependenices are listed in the install_scripts directory's README if you want to install them manually.\n\nPress any key to continue or ESC to exit...\033[0m"
 
 readc key
 
@@ -31,6 +31,10 @@ case "$key" in
   "$(printf '%b' '\033')")
   echo -e "\nExiting...\n";;
   *)
+
+        echo -e "\n\e[1;33mInitiliazing Git Submodules...\033[0m"
+        git submodule update --init --recursive
+
         if [[ "$(python3 --version 2>&1)" == *"not found"* ]]; then
             echo -e "\n\n\e[1;31mError: Python 3 not found!\n\nPyton 3.9 or higher is recommended for this project.\033[0m"
             exit
@@ -46,6 +50,9 @@ case "$key" in
         sudo apt-get -y update
         #sudo rm mosquitto-repo.gpg.key
         sudo apt-get -y install mosquitto mosquitto-clients
+
+        echo -e "\n\n\e[1;33mInstalling opencv package...\e[1;33m"
+        sudo apt-get install -y python3-opencv
 
         echo -e "\n\n\e[1;33mMaking backup of current Mosquitto configuration file...\e[1;33m"
         sudo mv /etc/mosquitto/mosquitto.conf /etc/mosquitto/mosquitto.conf_backup
@@ -68,11 +75,16 @@ case "$key" in
 
         pip install paho-mqtt pymongo
 
+        echo -e "\n\n\e[1;33mUpdating PIP...\033[0m"
+        python3 -m pip install pip --upgrade
+        
         echo -e "\n\n\e[1;33mInstalling Raspberry Pi-Specific dependencies...\033[0m"
-        pip install Adafruit-BMP Adafruit-DHT smbus
+        pip install argparse numpy opencv-python tflite-support protobuf        
 
-        echo -e "\n\n\e[1;33mEnabling I2C...\033[0m"
-        sudo raspi-config nonint do_i2c 0
+        echo -e "\n\n\e[1;33mDownloading TF Models...\033[0m"
+        curl -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/lite-model_efficientdet_lite0_detection_metadata_1.tflite' -o ../../TFLite/lite/examples/object_detection/raspberry_pi/efficientdet_lite0.tflite
+        curl -L 'https://storage.googleapis.com/download.tensorflow.org/models/tflite/task_library/object_detection/rpi/efficientdet_lite0_edgetpu_metadata.tflite' -o ../../TFLite/lite/examples/object_detection/raspberry_pi/efficientdet_lite0_edgetpu.tflite
+
 
         echo -e "\e[1;32m\n\nAll Done!\n\033[0m";;
 esac
